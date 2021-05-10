c0610e562e1f89868c0f55390702c92aa00c2a91
Jamie Kitson <jamie@kitten-x.com>
2021-05-10 10:45:34 +0100

    fix time stamps on files

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+git log --pretty=%at --name-status --reverse | perl -ane '($x,$f)=@F;next if !$x;$t=$x,next if !defined($f)||$s{$f};$s{$f}=utime($t,$t,$f),next if $x=~/[AM]/;'
+
 ikiwiki --setup geekout.org.uk.setup
